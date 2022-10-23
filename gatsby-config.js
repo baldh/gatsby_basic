4795b52b40e54dd1ac9f494dbb53e470a5116027
baldh <baldhokri1@gmail.com>
2022-10-23 20:14:08 +0530

    add site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://yourdomain.tld",
+    title: "Basic Gatsby Starter",
+    description: "Basic Gatsby example and setup",
+    image: "https://source.unsplash.com/YFzPw-Ph1Hw"
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,28 @@
 import * as React from "react"
+import {Link, useStaticQuery, graphql} from "gatsby"
 
 export default function IndexPage() {
-  return(
-    <main>
-      <h1>Welcome to gatsby</h1>
-    </main>
+  const data = useStaticQuery(graphql`
+      query MyQuery {
+          site {
+              siteMetadata {
+                  title
+              }
+          }
+      }
+  `)
+
+  const meta = data?.site?.siteMetadata ?? {}
+  return (
+    <>
+      <header>
+        <Link to={"/"}>{meta.title}</Link>
+      </header>
+      <main>
+        <h1>Welcome to gatsby</h1>
+        <Link to={"/about"}>Go yo about page</Link>
+      </main>
+    </>
+
   )
 }

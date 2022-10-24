3f0febfea9e7cb0ea70c56c370cd284735d721de
baldh <baldhokri1@gmail.com>
2022-10-24 19:26:15 +0530

    add and use a shared layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,11 @@
 import * as React from 'react';
-import { graphql, Link, useStaticQuery } from 'gatsby';
-import { Seo } from '../components/seo.js';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default function IndexPage() {
-  const data = useStaticQuery(graphql`
-    query GetSiteTitle {
-      site {
-        siteMetadata {
-          title
-          image
-        }
-      }
-    }
-  `);
-
-  const meta = data?.site?.siteMetadata ?? {};
-
   return (
-    <>
-      <Seo />
-      <header>
-        <Link to={'/'}>{meta.title}</Link>
-      </header>
-      <main>
-        <h1>Welcome to gatsby</h1>
-        <Link to={'/about'}>Go yo about page</Link>
-      </main>
-    </>
+    <Layout>
+      <h1>Welcome to gatsby</h1>
+    </Layout>
   );
 }
